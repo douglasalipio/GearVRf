08c9672ff3be4cc7d85a3cd1ced61704b78a012e
Nathan Almeida <nathan.a@samsung.com>
2016-01-14 14:20:59 -0400

    Implement subtitles
    Signed-off-by: Nathan Almeida <nathan.a@samsung.com>

--- GVRf/Framework/src/org/gearvrf/accessibility/GVRAccessibilityCaptions.java
+++ GVRf/Framework/src/org/gearvrf/accessibility/GVRAccessibilityCaptions.java
@@ -1,9 +1,30 @@
+/*
+ * Copyright [2016] [SIDIA Samsung Instituto de Informatica da Amazonia]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.gearvrf.accessibility;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.view.Gravity;
+
 import org.gearvrf.GVRContext;
+import org.gearvrf.GVRRenderData.GVRRenderMaskBit;
 import org.gearvrf.GVRSceneObject;
-import org.gearvrf.GVRTexture;
+import org.gearvrf.periodic.GVRPeriodicEngine.PeriodicEvent;
+import org.gearvrf.scene_objects.GVRViewSceneObject;
 import org.gearvrf.scene_objects.view.GVRTextView;
 
 /**
@@ -11,32 +32,171 @@ import org.gearvrf.scene_objects.view.GVRTextView;
  * position, will have camera rig as its parent and will appear for a fixed
  * amount of time when requested by {@link GVRSceneObject}.
  */
-public class GVRAccessibilityCaptions extends GVRSceneObject {
+public class GVRAccessibilityCaptions {
+    private static final float VIEW_POSITION_Z = -.8f; // -.3f
+    private static final float VIEW_POSITION_Y = .3333333f * VIEW_POSITION_Z; // -.1f
+    private static final float VIEW_POSITION_X = 0f;
+    private static final float VIEW_WIDTH = -.7666666f * VIEW_POSITION_Z; // .23f
+    private static final float VIEW_HEIGHT = -.0416666f * VIEW_POSITION_Z; // .10f
 
-    private GVRTextView subtitle;
-    private float captions_x;
-    private float captions_y;
-    private float captions_z;
-    private float showTime;
+    private static GVRAccessibilityCaptions sInstance;
 
-    public GVRAccessibilityCaptions(GVRContext gvrContext, float width, float height, GVRTexture texture) {
-        super(gvrContext, width, height, texture);
+    public static final float INFINITE_DURATION = -1;
+    private float duration = INFINITE_DURATION;
+    private PeriodicEvent event;
+
+    private GVRContext mGvrContext;
+    private GVRTextView mTextViewR;
+    private GVRTextView mTextViewL;
+    private GVRViewSceneObject mViewSceneObjectR;
+    private GVRViewSceneObject mViewSceneObjectL;
+    private boolean isActive = true;
+
+    public static synchronized GVRAccessibilityCaptions getInstance(GVRContext gvrContext) {
+        if (sInstance == null) {
+            sInstance = new GVRAccessibilityCaptions(gvrContext);
+        }
+        return sInstance;
     }
 
-    private void createCaptions() {
+    private GVRAccessibilityCaptions(GVRContext gvrContext) {
+        mGvrContext = gvrContext;
+        mGvrContext.getActivity().runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                mTextViewR = new GVRTextView(mGvrContext.getActivity());
+                configureGVRTextView(mTextViewR);
+
+                mTextViewL = new GVRTextView(mGvrContext.getActivity());
+                configureGVRTextView(mTextViewL);
+
+                mGvrContext.runOnGlThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        mViewSceneObjectR = new GVRViewSceneObject(mGvrContext, mTextViewR, VIEW_WIDTH, VIEW_HEIGHT);
+                        mViewSceneObjectR.getRenderData().setRenderMask(GVRRenderMaskBit.Right);
+                        mViewSceneObjectR.getTransform().setPosition(VIEW_POSITION_X, VIEW_POSITION_Y, VIEW_POSITION_Z);
+
+                        mViewSceneObjectL = new GVRViewSceneObject(mGvrContext, mTextViewL, VIEW_WIDTH, VIEW_HEIGHT);
+                        mViewSceneObjectL.getRenderData().setRenderMask(GVRRenderMaskBit.Left);
+                        mViewSceneObjectL.getTransform().setPosition(VIEW_POSITION_X, VIEW_POSITION_Y, VIEW_POSITION_Z);
+                    }
+                });
+            }
+
+            private void configureGVRTextView(final GVRTextView textView) {
+                textView.setBackgroundColor(Color.BLACK);
+                textView.setTextColor(Color.WHITE);
+
+                textView.setTextSize(30);
+                textView.setGravity(Gravity.CENTER_HORIZONTAL);
+                textView.setPadding(0, -90, 0, 0);
+                Typeface type = Typeface.createFromAsset(mGvrContext.getActivity().getAssets(), "fonts/DroidSans-Bold.ttf");
+                textView.setTypeface(type);
+            }
+
+        });
 
     }
 
-    public void setSubtitle(String text) {
-        this.subtitle.setText(text);
+    public GVRTextView getTextView() {
+        return mTextViewR;
     }
 
-    private void turnOn() {
-
+    public GVRViewSceneObject getViewSceneObject() {
+        return mViewSceneObjectR;
     }
 
-    private void turnOff() {
+    public CharSequence getText() {
+        return mTextViewR.getText();
+    }
 
+    public void setText(final CharSequence text) {
+        if (isActive) {
+            mGvrContext.getMainScene().getMainCameraRig().getRightCamera().addChildObject(mViewSceneObjectR);
+            mGvrContext.getMainScene().getMainCameraRig().getLeftCamera().addChildObject(mViewSceneObjectL);
+
+            removeSubtitlesAfterDuration();
+
+            mGvrContext.getActivity().runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+                    updateText(text, mTextViewR, mViewSceneObjectR);
+                    updateText(text, mTextViewL, mViewSceneObjectL);
+                }
+
+                private void updateText(final CharSequence text, final GVRTextView textView, final GVRViewSceneObject viewSceneObject) {
+                    textView.setText(text);
+                    final int lines = textView.getLineCount();
+                    mGvrContext.runOnGlThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            viewSceneObject.getRenderData().setMesh(mGvrContext.createQuad(VIEW_WIDTH, VIEW_HEIGHT * lines));
+                            textView.post(new Runnable() {
+
+                                @Override
+                                public void run() {
+                                    // workaround because sometimes
+                                    // textView.getLineCount() returns 0
+                                    final int currentLines = textView.getLineCount();
+                                    int correctLines = Math.max(lines, currentLines);
+                                    if (correctLines > 0) {
+                                        textView.setTextSize(240 / correctLines);
+                                        textView.setTextScaleX(correctLines / 8f);
+                                    }
+                                }
+
+                            });
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+    private void removeSubtitlesAfterDuration() {
+        if (event != null) {
+            event.cancel();
+        }
+        if (duration != INFINITE_DURATION) {
+            event = mGvrContext.getPeriodicEngine().runAfter(new Runnable() {
+
+                @Override
+                public void run() {
+                    mGvrContext.getMainScene().getMainCameraRig().getRightCamera().removeChildObject(mViewSceneObjectR);
+                    mGvrContext.getMainScene().getMainCameraRig().getLeftCamera().removeChildObject(mViewSceneObjectL);
+                    event.cancel();
+                    event = null;
+                }
+            }, duration);
+        }
+    }
+
+    public void setSubtitleDuration(float duration) {
+        this.duration = duration;
+        removeSubtitlesAfterDuration();
+    }
+
+    public float getSubtitleDuration() {
+        return duration;
+    }
+
+    public void show() {
+        isActive = true;
+        setText(getText());
+    }
+
+    public void hide() {
+        isActive = false;
+        if (event != null) {
+            event.cancel();
+        }
+        mGvrContext.getMainScene().getMainCameraRig().getRightCamera().removeChildObject(mViewSceneObjectR);
+        mGvrContext.getMainScene().getMainCameraRig().getLeftCamera().removeChildObject(mViewSceneObjectL);
     }
 
 }
