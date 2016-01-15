e0ccda93829e9df49f8b3590f5c43423630f8344
Nathan Almeida <nathan.a@samsung.com>
2016-01-15 14:49:24 -0400

    Create Menu Scene
    Signed-off-by: Nathan Almeida <nathan.a@samsung.com>

---
+++ GVRf/Framework/src/org/gearvrf/accessibility/GVRAccessibilityItem.java
@@ -0,0 +1,15 @@
+
+package org.gearvrf.accessibility;
+
+import org.gearvrf.GVRContext;
+import org.gearvrf.GVRMesh;
+import org.gearvrf.GVRSceneObject;
+import org.gearvrf.GVRTexture;
+
+public class GVRAccessibilityItem extends GVRSceneObject {
+
+    public GVRAccessibilityItem(GVRContext gvrContext, GVRMesh mesh, GVRTexture texture) {
+        super(gvrContext, mesh, texture);
+    }
+
+}
