f68125354022a8c80ff5b30e2da87e07b610692c
Nathan Almeida <nathan.a@samsung.com>
2016-01-18 09:32:06 -0400

    Animate menu elements by looking at them
    Signed-off-by: Nathan Almeida <nathan.a@samsung.com>

--- GVRf/Framework/src/org/gearvrf/accessibility/GVRAccessibilityItem.java
+++ GVRf/Framework/src/org/gearvrf/accessibility/GVRAccessibilityItem.java
@@ -5,11 +5,71 @@ import org.gearvrf.GVRContext;
 import org.gearvrf.GVRMesh;
 import org.gearvrf.GVRSceneObject;
 import org.gearvrf.GVRTexture;
+import org.gearvrf.animation.GVRAnimation;
+import org.gearvrf.animation.GVROnFinish;
+import org.gearvrf.animation.GVRRelativeMotionAnimation;
+import org.gearvrf.animation.GVRRotationByAxisAnimation;
 
 public class GVRAccessibilityItem extends GVRSceneObject {
+    private boolean isActive = false;
+    private boolean isAnimating = false;
+    private static final float duration = 0.35f;
 
     public GVRAccessibilityItem(GVRContext gvrContext, GVRMesh mesh, GVRTexture texture) {
         super(gvrContext, mesh, texture);
+    }
+
+    public void click() {
+        if (!isAnimating) {
+            isAnimating = true;
+            if (isActive) {
+                new GVRRelativeMotionAnimation(this, duration, 0, 0, -2).start(getGVRContext().getAnimationEngine()).setOnFinish(new GVROnFinish() {
+
+                    @Override
+                    public void finished(GVRAnimation animation) {
+                        new GVRRotationByAxisAnimation(GVRAccessibilityItem.this, duration, 180, 0, 1, 0).start(getGVRContext().getAnimationEngine())
+                                .setOnFinish(new GVROnFinish() {
+
+                                    @Override
+                                    public void finished(GVRAnimation animation) {
+                                        new GVRRelativeMotionAnimation(GVRAccessibilityItem.this, duration, 0, 0, 2).start(
+                                                getGVRContext().getAnimationEngine()).setOnFinish(new GVROnFinish() {
+
+                                            @Override
+                                            public void finished(GVRAnimation animation) {
+                                                isAnimating = false;
+                                            }
+                                        });
+                                    }
+                                });
+                    }
+                });
+            } else {
+                new GVRRelativeMotionAnimation(this, duration, 0, 0, -2).start(getGVRContext().getAnimationEngine()).setOnFinish(new GVROnFinish() {
+
+                    @Override
+                    public void finished(GVRAnimation animation) {
+                        new GVRRotationByAxisAnimation(GVRAccessibilityItem.this, duration, -180, 0, 1, 0)
+                                .start(getGVRContext().getAnimationEngine())
+                                .setOnFinish(new GVROnFinish() {
+
+                                    @Override
+                                    public void finished(GVRAnimation animation) {
+                                        new GVRRelativeMotionAnimation(GVRAccessibilityItem.this, duration, 0, 0, 2).start(
+                                                getGVRContext().getAnimationEngine()).setOnFinish(new GVROnFinish() {
+
+                                            @Override
+                                            public void finished(GVRAnimation animation) {
+                                                isAnimating = false;
+                                            }
+                                        });
+                                    }
+                                });
+                    }
+                });
+            }
+            isActive = !isActive;
+        }
     }
 
 }
