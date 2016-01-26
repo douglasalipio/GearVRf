e975a4c196a6979e5c86b030d5651b4add3e17a6
Douglas Alipio Mesquita <d.alipio@sidiamao.com.br>
2016-01-26 09:27:02 -0400

    Added news icons.

--- GVRf/Framework/src/org/gearvrf/accessibility/GVRAccessibilityMenu.java
+++ GVRf/Framework/src/org/gearvrf/accessibility/GVRAccessibilityMenu.java
@@ -10,55 +10,158 @@ import org.gearvrf.GVRMesh;
 import org.gearvrf.GVRSceneObject;
 import org.gearvrf.GVRTexture;
 import org.gearvrf.R;
+import org.gearvrf.scene_objects.GVRViewSceneObject;
+import org.gearvrf.scene_objects.view.GVRTextView;
 
 public class GVRAccessibilityMenu extends GVRSceneObject {
 
-    private List<GVRSceneObject> menuItems;
+    private List<GVRAccessibilityMenuItem> menuItems;
     private GVRContext mGvrContext;
+    private GVRViewSceneObject placeHolderBack;
+    private GVRTextView backText;
+    private GVRTexture mSpacerTexture;
+    private GVRMesh mSlotMesh;
+    private GVRTexture mSlotTexture;
 
     public GVRAccessibilityMenu(GVRContext gvrContext) {
         super(gvrContext);
 
         mGvrContext = gvrContext;
+        mSlotMesh = mGvrContext.loadMesh(new GVRAndroidResource(mGvrContext, R.raw.circle_menu));
+        mSlotTexture = mGvrContext.loadTexture(new GVRAndroidResource(mGvrContext, R.drawable.circle_normal));
+        mSpacerTexture = mGvrContext.loadTexture(new GVRAndroidResource(mGvrContext, R.drawable.circle_normal_alpha));
+        menuItems = new ArrayList<GVRAccessibilityMenuItem>();
+
+        createAllButtons();
         createMenuItems();
-        createBackButton();
+
     }
 
     private void createMenuItems() {
-        GVRMesh slot1Mesh = mGvrContext.loadMesh(new GVRAndroidResource(mGvrContext, R.raw.circle_menu));
-        GVRTexture slot1Texture = mGvrContext.loadTexture(new GVRAndroidResource(mGvrContext, R.drawable.circle_normal));
-        GVRTexture backTexture = mGvrContext.loadTexture(new GVRAndroidResource(mGvrContext, R.drawable.circle_normal_alpha));
-        menuItems = new ArrayList<GVRSceneObject>();
         for (int i = 0; i < 8; i++) {
             float degreeNormal = 360.0f * i / (8);
-            GVRAccessibilityMenuItem menuItem = new GVRAccessibilityMenuItem(mGvrContext, slot1Mesh, slot1Texture);
-            menuItems.add(menuItem);
-            menuItem.getTransform().setPosition(0, -1f, 0);
-            menuItem.getTransform().rotateByAxis(degreeNormal, 0, 1, 0);
-            addChildObject(menuItem);
-
-            if (i == 3) {
-                menuItem.setOnAction(new GVRAccessibilityOnAction() {
-
-                    @Override
-                    public void setOnAction() {
-                        getGVRContext().setMainScene(GVRAccessibilityScene.mainSceneApplication);
-                    }
-                });
-                menuItem.getRenderData().getMaterial().setMainTexture(backTexture);
-                menuItem.attachEyePointeeHolder();
-
-            }
+            menuItems.get(i).getTransform().setPosition(0, -1f, 0);
+            menuItems.get(i).getTransform().rotateByAxis(degreeNormal, 0, 1, 0);
+            addChildObject(menuItems.get(i));
         }
     }
 
+    private void createAllButtons() {
+        createBackButton();
+        createCaptionsButton();
+        createGazeButton();
+        createInvertedColorsButton();
+        createSpeechButton();
+        createTalkBackButton();
+        createZoomButton();
+        createDefaultItem();
+    }
+
+    private void createBackIcon() {
+        GVRSceneObject iconButton = new GVRSceneObject(mGvrContext, mGvrContext.createQuad(.2f, .3f),
+                mGvrContext.loadTexture(new GVRAndroidResource(mGvrContext, R.drawable.back_ico)));
+        iconButton.getTransform().setPosition(-.4f, -.99f, -.7f);
+        iconButton.getTransform().rotateByAxis(-90, 1, 0, 0);
+        iconButton.getTransform().rotateByAxis(35, 0, 1, 0);
+        addChildObject(iconButton);
+    }
+
     private void createBackButton() {
-        GVRSceneObject backButton = new GVRSceneObject(mGvrContext, mGvrContext.createQuad(.3f, .3f),
-                mGvrContext.loadTexture(new GVRAndroidResource(mGvrContext, R.drawable.accessibility_back_btn)));
-        backButton.getTransform().setPosition(-.2f, -.9f, -.7f);
-        backButton.getTransform().rotateByAxis(-90, 1, 0, 0);
-        backButton.getTransform().rotateByAxis(35, 0, 1, 0);
-        addChildObject(backButton);
+        GVRAccessibilityMenuItem backButton = new GVRAccessibilityMenuItem(mGvrContext, mSlotMesh, mSlotTexture);
+        backButton.getRenderData().getMaterial().setMainTexture(mSpacerTexture);
+        backButton.setOnAction(new GVRAccessibilityOnAction() {
+
+            @Override
+            public void setOnAction() {
+                getGVRContext().setMainScene(GVRAccessibilityManager.mMainSceneApplication);
+            }
+        });
+
+        menuItems.add(backButton);
+    }
+
+    private void createCaptionsButton() {
+        GVRAccessibilityMenuItem captionsButton = new GVRAccessibilityMenuItem(mGvrContext, mSlotMesh, mSlotTexture);
+        captionsButton.getRenderData().getMaterial().setMainTexture(mSpacerTexture);
+        captionsButton.setOnAction(new GVRAccessibilityOnAction() {
+
+            @Override
+            public void setOnAction() {
+            }
+        });
+
+        menuItems.add(captionsButton);
+    }
+
+    private void createGazeButton() {
+        GVRAccessibilityMenuItem gazeButton = new GVRAccessibilityMenuItem(mGvrContext, mSlotMesh, mSlotTexture);
+        gazeButton.getRenderData().getMaterial().setMainTexture(mSpacerTexture);
+        gazeButton.setOnAction(new GVRAccessibilityOnAction() {
+
+            @Override
+            public void setOnAction() {
+            }
+        });
+
+        menuItems.add(gazeButton);
+    }
+
+    private void createInvertedColorsButton() {
+        GVRAccessibilityMenuItem InvertedColorsButton = new GVRAccessibilityMenuItem(mGvrContext, mSlotMesh, mSlotTexture);
+        InvertedColorsButton.getRenderData().getMaterial().setMainTexture(mSpacerTexture);
+        InvertedColorsButton.setOnAction(new GVRAccessibilityOnAction() {
+
+            @Override
+            public void setOnAction() {
+            }
+        });
+
+        menuItems.add(InvertedColorsButton);
+    }
+
+    private void createZoomButton() {
+        GVRAccessibilityMenuItem zoomButton = new GVRAccessibilityMenuItem(mGvrContext, mSlotMesh, mSlotTexture);
+        zoomButton.getRenderData().getMaterial().setMainTexture(mSpacerTexture);
+        zoomButton.setOnAction(new GVRAccessibilityOnAction() {
+
+            @Override
+            public void setOnAction() {
+            }
+        });
+
+        menuItems.add(zoomButton);
+    }
+
+    private void createSpeechButton() {
+        GVRAccessibilityMenuItem SpeechButton = new GVRAccessibilityMenuItem(mGvrContext, mSlotMesh, mSlotTexture);
+        SpeechButton.getRenderData().getMaterial().setMainTexture(mSpacerTexture);
+        SpeechButton.setOnAction(new GVRAccessibilityOnAction() {
+
+            @Override
+            public void setOnAction() {
+            }
+        });
+
+        menuItems.add(SpeechButton);
+    }
+
+    private void createTalkBackButton() {
+        GVRAccessibilityMenuItem talkBackButton = new GVRAccessibilityMenuItem(mGvrContext, mSlotMesh, mSlotTexture);
+        talkBackButton.getRenderData().getMaterial().setMainTexture(mSpacerTexture);
+        talkBackButton.setOnAction(new GVRAccessibilityOnAction() {
+
+            @Override
+            public void setOnAction() {
+            }
+        });
+
+        menuItems.add(talkBackButton);
+    }
+
+    private void createDefaultItem() {
+        GVRAccessibilityMenuItem defaultItem = new GVRAccessibilityMenuItem(mGvrContext, mSlotMesh, mSlotTexture);
+        defaultItem.getRenderData().getMaterial().setMainTexture(mSpacerTexture);
+        menuItems.add(defaultItem);
     }
 
 }
